747761599df4d9600bce189635f8422ab4deb88d
Bill Baggins <billyolebaggins@gmail.com>
2020-11-14 12:18:05 -0600

    -changed database from sqlite3 to postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bills_site_db',
+        'USER': 'bill',
+        'PASSWORD': 'IHateCookiesAndCream',
+        'HOST': 'localhost',
+        'PORT': '', 
     }
 }
 
