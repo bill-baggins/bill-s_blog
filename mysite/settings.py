669ec13cce48ec176687dc41bd96a5d864d57708
Bill Baggins <billyolebaggins@gmail.com>
2020-11-15 19:30:06 -0600

    -added psycopg to requriements.txt; put '.azurewebsites.net' in allowed_hosts in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bo7qfuxm4wuqg#whk)tp2dpa=9!=#r@lcph-asqieot(f_vckh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',".azurewebsites.net"]
 
 
 # Application definition
